ede46311f7d274938685350a70f8ec3f2ebd05a2
saboooor <saboor547@gmail.com>
2022-11-04 23:57:15 -0600

    fix embedding and description

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -11,14 +11,15 @@ export const RouterHead = component$(() => {
   return (
     <>
       <title>Burgers on Fleek - {head.title}</title>
+      <meta content="Premium Quality Gourmet Burgers, Steak Sandwiches, Fries, and more. Only serving Halal حلال" name="description" />
 
       <link rel="canonical" href={loc.href} />
       <link rel="icon" href="icon.svg" />
       <link rel="mask-icon" href="mask-icon.svg" color="#c37f0c" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta content="Burgers On Fleek" property="og:title" />
-      <meta content="Premium Quality Gourmet Burgers, Steak Sandwiches, Fries, and more. Only serving Halal حلال" property="og:description" name="description" />
-      <meta content="logo.png" property="og:image" />
+      <meta content="Premium Quality Gourmet Burgers, Steak Sandwiches, Fries, and more. Only serving Halal حلال" property="og:description" />
+      <meta content="https://burgersonfleek.ca/logo.png" property="og:image" />
       <meta content="#c37f0c" name="theme-color" />
 
       {head.meta.map((m) => (
