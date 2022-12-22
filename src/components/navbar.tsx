8abaf1a7d5572bfa810ac981057640d50e76b4e4
saboooor <saboor547@gmail.com>
2022-12-22 00:23:23 -0700

    SPA

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
@@ -6,16 +7,16 @@ export default component$(() => {
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
           <div class="flex flex-1 items-center sm:items-stretch justify-start">
-            <a href="/" class="transition duration-200 pointer-events-auto text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl pl-2 pr-3 py-2 rounded-full text-sm font-medium flex items-center whitespace-nowrap">
+            <Link href="/" class="transition duration-200 pointer-events-auto text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl pl-2 pr-3 py-2 rounded-full text-sm font-medium flex items-center whitespace-nowrap">
               <img class="h-8 w-8 mr-3" src={`/icon.svg`} alt="Burgers On Fleek" />
               Burgers on Fleek
-            </a>
+            </Link>
           </div>
           <div class="flex flex-1 items-center justify-end sm:items-stretch">
             <div class="sm:ml-6 sm:block">
               <div class="pointer-events-auto flex space-x-4">
-                <a href="/" class="transition duration-200 hidden sm:flex text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl px-3 py-2 rounded-full text-sm font-medium">Home</a>
-                <a href="/menu" class="transition duration-200 text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl px-3 py-2 rounded-full text-sm font-medium">Menu</a>
+                <Link href="/" class="transition duration-200 hidden sm:flex text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl px-3 py-2 rounded-full text-sm font-medium">Home</Link>
+                <Link href="/menu" class="transition duration-200 text-gray-300 bg-gray-900 hover:bg-gray-800 hover:text-white hover:drop-shadow-2xl px-3 py-2 rounded-full text-sm font-medium">Menu</Link>
               </div>
             </div>
           </div>
