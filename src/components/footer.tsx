b5d22abd22a7ed2eaf1d9dad601f3d38691be6d5
saboooor <saboor547@gmail.com>
2023-02-20 19:07:58 -0700

    move copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,9 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <footer class="text-center text-gray-400 h-16">
-      <p>© Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.</p>
       <p>All of our food is cooked to the internal temperature required by the CFIA.</p>
       <p>If you have any food allergies, please inquire before ordering.</p>
+      <p>© Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.</p>
     </footer>
   );
 });
