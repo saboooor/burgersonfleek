a3298637390a0bb7c7be584e95382d08fc23e006
saboooor <saboor547@gmail.com>
2023-02-20 19:22:38 -0700

    more footer changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,10 +2,15 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <footer class="text-center text-gray-400 py-5 text-xs md:text-sm">
-      <p>All of our food is cooked to the internal temperature required by the CFIA.</p>
-      <p>If you have any food allergies, please inquire before ordering.</p><br/>
-      <p>© Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.</p>
+    <footer class="text-center text-gray-400 py-5 px-2">
+      <p class="text-lg md:text-xl">
+        All of our food is cooked to the internal temperature required by the CFIA.<br class="hidden md:block"/>
+        If you have any food allergies, please inquire before ordering.
+      </p>
+      <br/>
+      <p class="text-xs md:text-sm">
+        © Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.
+      </p>
     </footer>
   );
 });
