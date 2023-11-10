642d6b2c9882fb6dba9eedd4a3970e392c77c462
saboooor <saboor547@gmail.com>
2023-11-10 13:12:50 -0700

    better blobs and mobile hero and orange hue in background

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,32 +3,33 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 import { Link } from '@builder.io/qwik-city';
 
 import Logo from '~/components/svg/Logo';
-import IconWhite from '~/components/svg/IconInBag';
+import Icon from '~/components/svg/Icon';
+import IconInBag from '~/components/svg/IconInBag';
 import { LogoInstagram, LogoFacebook, BookOutline, CallOutline, MapOutline } from 'qwik-ionicons';
 
 export default component$(() => {
   return (
     <section class="flex mx-auto max-w-6xl px-6 items-center justify-center min-h-[calc(100lvh)] pt-22 sm:pt-28">
       <div class="hidden sm:flex relative justify-start align-center mr-auto" style="max-width: 50%;">
-        <div class="absolute top-10 left-10 w-72 h-72 bg-orange-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl"></div>
-        <div class="absolute top-10 right-10 w-72 h-72 bg-amber-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl animation-delay-2000"></div>
-        <div class="absolute bottom-5 left-32 w-72 h-72 bg-yellow-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl animation-delay-4000"></div>
+        <div class="absolute left-0 right-0 mx-auto bottom-24 w-48 h-48 bg-orange-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl"></div>
+        <div class="absolute left-0 right-0 mx-auto bottom-24 w-48 h-48 bg-amber-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl animation-delay-5"></div>
+        <div class="absolute left-0 right-0 mx-auto bottom-24 w-48 h-48 bg-yellow-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-2xl animation-delay-10"></div>
         <div class="z-10 animate-float ease-in-out" style="max-width: 60%" >
           <Logo width={476} height={600} id="pc"/>
         </div>
       </div>
       <div class="text-center justify-center">
-        <div class="flex sm:hidden relative justify-center align-center mb-10" style="width: 100%;">
-          <div class="absolute top-10 w-32 h-32 bg-orange-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-xl" style="left: 45%"></div>
-          <div class="absolute top-10 w-32 h-32 bg-amber-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-xl animation-delay-2000" style="right: 50%"></div>
-          <div class="absolute bottom-5 w-32 h-32 bg-yellow-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-xl animation-delay-4000" style="left: 40%"></div>
+        <div class="flex sm:hidden relative justify-center align-center" style="width: 100%;">
+          <div class="absolute -bottom-16 w-32 h-32 bg-orange-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-lg" style="left: 45%"></div>
+          <div class="absolute -bottom-16 w-32 h-32 bg-amber-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-lg animation-delay-5" style="right: 50%"></div>
+          <div class="absolute -bottom-16 w-32 h-32 bg-yellow-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-lg animation-delay-10" style="left: 40%"></div>
           <div class="z-10">
-            <h1 class="font-bold text-orange-100 text-4xl font-futura tracking-wider" style="filter: drop-shadow(0 2rem 2rem rgba(251, 146, 60, 0.5));">
-              The <span class="text-amber-500">burgers</span><br/>you are <span class="text-orange-300">craving.</span>
-            </h1>
+            <div class="z-10 animate-float ease-in-out">
+              <Icon width={119} height={150} id="mobile"/>
+            </div>
           </div>
         </div>
-        <h1 class="hidden sm:block font-bold text-orange-100 text-5xl mb-6" style="filter: drop-shadow(0 2rem 2rem rgba(251, 146, 60, 0.5));">
+        <h1 class="font-bold text-orange-100 text-3xl sm:text-5xl mb-6" style="filter: drop-shadow(0 2rem 2rem rgba(251, 146, 60, 0.5));">
           The <span class="text-amber-500">burgers</span><br/>you are <span class="text-orange-300">craving.</span>
         </h1>
         <p class="text-gray-400 text-lg mx-6 md:mx-20 md:text-xl">
@@ -40,7 +41,7 @@ export default component$(() => {
               <BookOutline width="24" class="fill-current" /> View the menu
             </Link>
             <a href="https://burgersonfleek.order.online/store/25869825" class="flex transition rounded-xl shadow-lg backdrop-blur-lg bg-gradient-to-b from-burger-100/80 to-burger-200/80 hover:bg-burger-100 px-6 py-3 font-bold text-red-100 md:py-4 md:px-8 text-sm md:text-lg whitespace-nowrap gap-3 items-center">
-              <IconWhite width="24" class="fill-current" /> Order Online
+              <IconInBag width="24" class="fill-current" /> Order Online
             </a>
           </div>
           <div class="flex gap-2 justify-center">
