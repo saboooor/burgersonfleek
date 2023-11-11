b3ea02bc794d727c29ec9638494cba3eaa78fbb5
saboooor <saboor547@gmail.com>
2023-11-10 18:30:39 -0700

    better color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,8 @@ import { Slot, component$ } from '@builder.io/qwik';
 export default component$(({ extraClass, row, squish, fit, darker, contextMenu }: any) => {
   return (
     <div class={{
-      'flex border border-gray-700 rounded-xl p-6': true,
-      'bg-gray-900/50': darker,
-      'bg-gray-800': !darker,
+      'flex border border-gray-700/80 rounded-xl p-6': true,
+      'bg-gray-850': !darker,
       'flex-col': !row,
       'flex-1 min-w-[18rem]': !squish,
       'min-w-fit': fit,
