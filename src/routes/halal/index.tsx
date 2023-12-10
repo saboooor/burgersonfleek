ec1513f1cca53954dcd80b532d4db7f7585ca18a
saboooor <saboor547@gmail.com>
2023-12-10 15:33:45 -0700

    some tweaks

--- src/routes/halal/index.tsx
+++ src/routes/halal/index.tsx
@@ -4,7 +4,7 @@ import Icon from '~/components/svg/Icon';
 
 export default component$(() => {
   return (
-    <section class="flex mx-auto max-w-6xl px-6 items-center justify-center min-h-[calc(100lvh)] pt-24">
+    <section class="flex mx-auto max-w-6xl px-6 items-center justify-center min-h-[calc(100lvh)] pt-20">
       <div class="text-center justify-center">
         <div class="flex relative justify-center align-center mb-10" style="width: 100%;">
           <div class="absolute -bottom-16 w-32 h-32 bg-orange-400 rounded-full opacity-10 animate-blob ease-in-out filter blur-xl"></div>
@@ -19,7 +19,7 @@ export default component$(() => {
         <h1 class="font-bold text-orange-100 text-2xl sm:text-5xl mb-8 sm:mb-16 fade-in animation-delay-100">
           We Only Serve <span class="text-orange-300">Halal حلال</span>
         </h1>
-        <div class="text-gray-400 text-xl mx-6 md:mx-20 md:text-2xl flex flex-col gap-2 mb-6 sm:mb-10 fade-in animation-delay-200">
+        <div class="text-gray-400 text-xl mx-6 md:mx-16 md:text-2xl flex flex-col gap-4 mb-6 sm:mb-10 fade-in animation-delay-200">
           <p>
             Burgers On Fleek uses 100% Hand slaughtered Halal Meat.
           </p>
@@ -28,14 +28,17 @@ export default component$(() => {
             so there is no chance of cross contamination.
           </p>
         </div>
-        <div class="text-gray-400 text-lg mx-6 md:mx-20 md:text-xl flex flex-col gap-2 fade-in animation-delay-300">
+        <div class="text-gray-400 text-lg mx-6 md:mx-16 md:text-xl mb-6 sm:mb-10 fade-in animation-delay-300">
           <p>
-            Our meat providers are <a href="http://sthelensmeat.com" class="text-blue-400/80 hover:text-blue-400 hover:underline">St. Helen's</a> and <a href="https://sargentfarms.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">Sargent Farms</a>
+            Our meat providers are
           </p>
           <p>
-            For further inquiries, feel free to reach us at <a href="mailto:eat@burgersonfleek.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">eat@burgersonfleek.ca</a>
+            <a href="http://sthelensmeat.com" class="text-blue-400/80 hover:text-blue-400 hover:underline">St. Helen's</a> and <a href="https://sargentfarms.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">Sargent Farms</a>
           </p>
         </div>
+        <p class="text-gray-400 text-base mx-6 md:mx-16 md:text-lg fade-in animation-delay-400">
+          For further inquiries, feel free to reach us at <a href="mailto:eat@burgersonfleek.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">eat@burgersonfleek.ca</a>
+        </p>
       </div>
     </section>
   );
