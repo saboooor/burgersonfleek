8e8c41f2b4f7ced467f6cb055b6ec48fcaec0a55
saboooor <saboor547@gmail.com>
2023-12-10 15:23:02 -0700

    capitalize footer

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,6 @@
 import { component$, Slot } from '@builder.io/qwik';
 import Nav from '~/components/Nav';
-import Footer from '~/components/Footerr';
+import Footer from '~/components/Footer';
 
 export default component$(() => {
   return (
