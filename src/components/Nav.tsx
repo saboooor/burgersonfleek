517a99eaad73b15aa1a1d550b2ee8fe54a92e82d
saboooor <saboor547@gmail.com>
2024-05-11 02:38:08 -0600

    update luminescent ui

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,7 +15,7 @@ export default component$(() => {
       'font-futura tracking-wider': true,
     }}>
       <Link q:slot="start" href="/">
-        <Button color="transparent">
+        <Button transparent>
           <Icon width={32} />
           <p class="mt-0.5 space-x-1 whitespace-nowrap">
             <span class="font-bold">BURGERS</span><span>ON</span><span>FLEEK</span>
@@ -30,15 +30,15 @@ export default component$(() => {
       </Link>
 
       <Link q:slot="end" href="/menu" class={{ 'hidden sm:flex': true }}>
-        <Button color="transparent" class={{ 'w-full': true }}>
+        <Button transparent class={{ 'w-full': true }}>
           <BookOutline width="24" class="fill-current" /> MENU
         </Button>
       </Link>
-      <ButtonAnchor q:slot="end" href="tel:+1 (905) 427 4377" color="transparent" class={{ 'hidden sm:flex': true }}>
+      <ButtonAnchor q:slot="end" href="tel:+1 (905) 427 4377" transparent class={{ 'hidden sm:flex': true }}>
         <IconInBag width="24" class="fill-current" /> ORDER
       </ButtonAnchor>
       <Link q:slot="end" href="/halal" class={{ 'hidden sm:flex': true }}>
-        <Button color="transparent" class={{ 'w-full': true }}>
+        <Button transparent class={{ 'w-full': true }}>
           <Halal width="24" class="fill-current" /> HALAL
         </Button>
       </Link>
@@ -47,15 +47,15 @@ export default component$(() => {
       </div>
 
       <Link q:slot="mobile" href="/menu">
-        <Button color="transparent" class={{ 'w-full': true }}>
+        <Button transparent class={{ 'w-full': true }}>
           <BookOutline width="24" class="fill-current" /> MENU
         </Button>
       </Link>
-      <ButtonAnchor q:slot="mobile" href="tel:+1 (905) 427 4377" color="transparent">
+      <ButtonAnchor q:slot="mobile" href="tel:+1 (905) 427 4377" transparent>
         <IconInBag width="24" class="fill-current" /> ORDER
       </ButtonAnchor>
       <Link q:slot="mobile" href="/halal">
-        <Button color="transparent" class={{ 'w-full': true }}>
+        <Button transparent class={{ 'w-full': true }}>
           <Halal width="24" class="fill-current" /> HALAL
         </Button>
       </Link>
@@ -69,16 +69,16 @@ export default component$(() => {
 
 export const SocialButtons = component$(() => {
   return <>
-    <ButtonAnchor color="transparent" square href="https://instagram.com/burgersonfleek.ca" title="Instagram">
+    <ButtonAnchor transparent square href="https://instagram.com/burgersonfleek.ca" title="Instagram">
       <LogoInstagram width="24" />
     </ButtonAnchor>
-    <ButtonAnchor color="transparent" square href="https://facebook.com/burgersonfleek.ca" title="Facebook">
+    <ButtonAnchor transparent square href="https://facebook.com/burgersonfleek.ca" title="Facebook">
       <LogoFacebook width="24" />
     </ButtonAnchor>
-    <ButtonAnchor color="transparent" square href="https://tiktok.com/@burgersonfleek.ca" title="TikTok">
+    <ButtonAnchor transparent square href="https://tiktok.com/@burgersonfleek.ca" title="TikTok">
       <LogoTiktok width="24" />
     </ButtonAnchor>
-    <ButtonAnchor color="transparent" square href="https://maps.app.goo.gl/Unrrg3uda7AQQs4DA" title="Google Maps">
+    <ButtonAnchor transparent square href="https://maps.app.goo.gl/Unrrg3uda7AQQs4DA" title="Google Maps">
       <LocationOutline width="24" />
     </ButtonAnchor>
   </>;
