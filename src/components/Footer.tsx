0001f8a16df7ef4c18364283b39c5d62fae606a1
saboooooor <saboor547@gmail.com>
2024-06-16 03:09:31 -0600

    chore: Update footer design and content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,25 +3,32 @@ import { SocialButtons } from './Nav';
 
 export default component$(() => {
   return (
-    <footer class="relative flex flex-col gap-1 items-center justify-center text-center w-full z-10 bg-gray-950 border-t border-t-gray-800 py-6 px-3">
-      <div class="flex gap-2 mb-2">
-        <SocialButtons />
-      </div>
+    <footer class="items-center justify-center z-10 bg-gray-950 border-t border-t-gray-800">
+      <div class="max-w-7xl flex flex-col sm:flex-row gap-2 sm:gap-8 items-center mx-auto py-8 px-2">
+        <p class="sm:text-xl sm:text-left text-center">
+          We are not fast food.
+          <br />
+          All our food is made fresh, and it can take a minimum of 15 minutes to complete your order.
+          <br />
+          All of our food is cooked to the internal temperature required by the CFIA.
+          <br />
+          If you have any food allergies, please inquire before ordering.
+        </p>
 
-      <p class="sm:text-xl mb-3">
-        We are not fast food.
-        <br />
-        All our food is made fresh, and it can take a minimum of 15 minutes to complete your order.
-        <br />
-        All of our food is cooked to the internal temperature required by the CFIA.
-        If you have any food allergies, please inquire before ordering.
-      </p>
-      <p class="text-xs md:text-sm">
-        Contact us at <a href="mailto:eat@burgersonfleek.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">eat@burgersonfleek.ca</a>
-      </p>
-      <p class="text-xs md:text-sm">
-        © Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.
-      </p>
+        <div class="flex flex-col text-center items-center sm:text-end sm:items-end gap-4">
+          <div class="flex gap-2">
+            <SocialButtons />
+          </div>
+          <div>
+            <p class="text-xs md:text-sm">
+              Contact us at <a href="mailto:eat@burgersonfleek.ca" class="text-blue-400/80 hover:text-blue-400 hover:underline">eat@burgersonfleek.ca</a>
+            </p>
+            <p class="text-xs md:text-sm">
+              © Copyright {new Date().getFullYear()} Burgers on Fleek - All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 });
