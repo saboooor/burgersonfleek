7695fc21a74c0dab46d0ebd96d4f4c736d141a32
Saboor <42164502+saboooor@users.noreply.github.com>
2025-05-15 04:40:58 -0600

    Update steaksandwiches.ts

--- src/components/menuitems/steaksandwiches.ts
+++ src/components/menuitems/steaksandwiches.ts
@@ -4,23 +4,23 @@ export default [
   {
     name: 'Lowkey',
     description: 'Thinly sliced AAA rib-eye steak with sautéed onions, lettuce, tomatoes, pickles, and On Fleek sauce.',
-    price: '$15.65',
+    price: '$15.80',
   },
   {
     name: 'Hookup',
     description: 'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, mushrooms, lettuce, tomatoes, pickles, melted cheese, and garlic mayo.',
-    price: '$18.65',
+    price: '$18.80',
     bestseller: true,
     image: Hookup,
   },
   {
     name: 'Hangry',
     description: 'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, topped with fried chicken and chipotle sauce.',
-    price: '$23.65',
+    price: '$23.80',
   },
   {
     name: 'Steak on Fries',
     description: 'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, chipotle sauce, and garlic mayo.',
-    price: '$19.15',
+    price: '$19.50',
   },
 ];
