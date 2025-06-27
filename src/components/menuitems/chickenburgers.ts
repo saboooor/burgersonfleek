a885230019d0336598e586e005a3fe5ead99532f
Saboor <42164502+saboooor@users.noreply.github.com>
2025-06-27 17:12:55 -0600

    Update chickenburgers.ts

--- src/components/menuitems/chickenburgers.ts
+++ src/components/menuitems/chickenburgers.ts
@@ -3,17 +3,17 @@ import Goat from '../images/Goat.png?jsx';
 export default [
   {
     name: 'Shawty',
-    description: 'Fried chicken thigh burger with lettuce, tomato, and chipotle sauce.',
+    description: 'Boneless fried chicken thigh burger with lettuce, tomato, and chipotle sauce.',
     price: '$10.25',
   },
   {
     name: 'The Original',
-    description: 'Fried chicken burger with lettuce, tomato, and chipotle sauce.',
+    description: 'Boneless fried chicken leg burger with lettuce, tomato, and chipotle sauce.',
     price: '$14.65',
   },
   {
     name: 'G.O.A.T',
-    description: 'Fried chicken leg burger with melted cheese, lettuce, tomato, roasted peppers, pico de gallo, chipotle sauce, and garlic mayo.',
+    description: 'Boneless fried chicken leg burger with melted cheese, lettuce, tomato, roasted peppers, pico de gallo, chipotle sauce, and garlic mayo.',
     price: '$17.90',
     bestseller: true,
     image: Goat,
