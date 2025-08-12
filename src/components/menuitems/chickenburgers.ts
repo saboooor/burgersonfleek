3a6a72c233ea6cf7a3a833e068106fb85cf1e0a1
saboooor <saboor547@gmail.com>
2025-08-11 19:00:30 -0600

    fix: update burger descriptions to remove 'boneless' references

--- src/components/menuitems/chickenburgers.ts
+++ src/components/menuitems/chickenburgers.ts
@@ -3,17 +3,17 @@ import Goat from '../images/Goat.png?jsx';
 export default [
   {
     name: 'Shawty',
-    description: 'Boneless fried chicken thigh burger with lettuce, tomato, and chipotle sauce.',
+    description: 'Fried chicken thigh burger with lettuce, tomato, and chipotle sauce.',
     price: '$10.25',
   },
   {
     name: 'The Original',
-    description: 'Boneless fried chicken leg burger with lettuce, tomato, and chipotle sauce.',
+    description: 'Fried chicken leg burger with lettuce, tomato, and chipotle sauce.',
     price: '$14.65',
   },
   {
     name: 'G.O.A.T',
-    description: 'Boneless fried chicken leg burger with melted cheese, lettuce, tomato, roasted peppers, pico de gallo, chipotle sauce, and garlic mayo.',
+    description: 'Fried chicken leg burger with melted cheese, lettuce, tomato, roasted peppers, pico de gallo, chipotle sauce, and garlic mayo.',
     price: '$17.90',
     bestseller: true,
     image: Goat,
