ca0513a6672b4c46770cb2ffc9dbf3351a5f5c19
saboooor <saboor547@gmail.com>
2026-04-08 00:09:58 -0400

    update qwik

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -1,24 +1,22 @@
 /*
  * WHAT IS THIS FILE?
  *
- * It's the  entry point for cloudflare-pages when building for production.
+ * It's the entry point for Cloudflare Pages when building for production.
  *
- * Learn more about the cloudflare integration here:
- * - https://qwik.builder.io/deployments/cloudflare-pages/
+ * Learn more about the Cloudflare Pages integration here:
+ * - https://qwik.dev/docs/deployments/cloudflare-pages/
  *
  */
 import {
   createQwikRouter,
   type PlatformCloudflarePages,
 } from '@qwik.dev/router/middleware/cloudflare-pages';
-import qwikRouterConfig from '@qwik-router-config';
-import { manifest } from '@qwik-client-manifest';
 import render from './entry.ssr';
 
 declare global {
-  type QwikCityPlatform = PlatformCloudflarePages
+  type QwikRouterPlatform = PlatformCloudflarePages;
 }
 
-const fetch = createQwikRouter({ render, qwikRouterConfig, manifest });
+const fetch = createQwikRouter({ render });
 
 export { fetch };
