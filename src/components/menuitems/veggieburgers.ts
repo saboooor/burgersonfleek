2d917f3b1cbca0544b8a3f3d6b82f8da97971e2a
Saboor <42164502+saboooor@users.noreply.github.com>
2026-06-27 19:51:54 -0400

    Update veggieburgers.ts

--- src/components/menuitems/veggieburgers.ts
+++ src/components/menuitems/veggieburgers.ts
@@ -5,13 +5,13 @@ export default [
   {
     name: 'The Veggie',
     description: 'Veggie patty with cheddar cheese, mushrooms, sautéed onions, lettuce, tomato, pickles, guacamole, pico de gallo, and chipotle sauce.',
-    price: '$13.49',
+    price: '$13.99',
     image: Veggie,
   },
   {
     name: 'Shroom',
     description: 'Crusted portobello mushroom stuffed with cheese, with lettuce, tomato, pickles, and garlic mayo.',
-    price: '$11.75',
+    price: '$11.99',
     image: Shroom,
   },
 ];
