bee77f7c73803339899e352ada5fa9d8d051ca39
saboooor <github@saboor.ca>
2026-08-02 17:21:29 -0400

    feat: enhance order display layout with logo and styling adjustments

--- src/routes/order-display/index@headless.tsx
+++ src/routes/order-display/index@headless.tsx
@@ -1,14 +1,20 @@
 import { component$ } from '@qwik.dev/core';
+import LogoHorizontal from '~/components/svg/LogoHorizontal';
 import { generateHead } from '~/root';
 
 export default component$(() => {
   return (
-    <iframe
-      class="rounded-lum h-dvh w-full mix-blend-screen"
-      src="https://www.toasttab.com/order-display"
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-    ></iframe>
+    <>
+      <div class="flex h-[20dvh] w-full items-center justify-center mix-blend-screen">
+        <LogoHorizontal size={600} height={250} id="order-display" />
+      </div>
+      <iframe
+        class="h-[80dvh] w-full mix-blend-screen"
+        src="https://www.toasttab.com/order-display"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
   );
 });
 
