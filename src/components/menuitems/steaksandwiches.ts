e66f094e266c0ec8ec77164993a9f967a24e1928
saboooor <github@saboor.ca>
2026-09-01 21:28:06 -0400

    fix: remove redundant 'AAA' from rib-eye steak descriptions in steaksandwiches component

--- src/components/menuitems/steaksandwiches.ts
+++ src/components/menuitems/steaksandwiches.ts
@@ -4,13 +4,13 @@ export default [
   {
     name: 'Lowkey',
     description:
-      'Thinly sliced AAA rib-eye steak with sautéed onions, lettuce, tomatoes, pickles, and On Fleek sauce.',
+      'Thinly sliced rib-eye steak with sautéed onions, lettuce, tomatoes, pickles, and On Fleek sauce.',
     price: '$15.99',
   },
   {
     name: 'Hookup',
     description:
-      'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, mushrooms, lettuce, tomatoes, pickles, melted cheese, and garlic mayo.',
+      'Thinly sliced rib-eye steak with sautéed onions, roasted peppers, mushrooms, lettuce, tomatoes, pickles, melted cheese, and garlic mayo.',
     price: '$18.99',
     bestseller: true,
     image: Hookup,
@@ -18,13 +18,13 @@ export default [
   {
     name: 'Hangry',
     description:
-      'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, topped with fried chicken thigh and chipotle sauce.',
+      'Thinly sliced rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, topped with fried chicken thigh and chipotle sauce.',
     price: '$23.99',
   },
   {
     name: 'Steak on Fries',
     description:
-      'Thinly sliced AAA rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, chipotle sauce, and garlic mayo.',
+      'Thinly sliced rib-eye steak with sautéed onions, roasted peppers, jalapeños, mushrooms, melted cheese, chipotle sauce, and garlic mayo.',
     price: '$19.99',
   },
 ];
